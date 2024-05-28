34b6cf4369e3e9998b60c934fc24eb82140f041a
Adam Ford <ford.adm@gmail.com>
2024-05-27 23:19:03 -0500

    add js to rothko html

---
+++ code.js
@@ -0,0 +1,20 @@
+
+let mean = "This is the meaning";
+
+let num = 12;
+let numAgain = 14;
+
+function addingFunc(num1, num2) {
+    let add = num1 + num2;
+    return add;
+}
+
+function multipFunc(num1, num2) {
+    let multi = num1 * num2;
+    return multi;
+}
+
+console.log(addingFunc(num, numAgain));
+console.log(multipFunc(num, numAgain));
+
+
